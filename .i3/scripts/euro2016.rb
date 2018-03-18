18b881f03a97ce708e1cc251ee65c5ccebf8ac0b
Xavier Lucas <xavier@systemdump.io>
2018-03-18 18:32:32 +0100

    Transition to py3status
    Signed-off-by: Xavier Lucas <xavier@systemdump.io>

--- .i3/scripts/euro2016.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-require_relative 'lib/i3block_handler'
-
-current = `euro2016 schedule status IN_PLAY | grep -m1 IN_PLAY | awk '{print $4" "$6" - "$8" "$10}'`.chomp()
-
-color = I3BlockHandler::COLOR_GREEN
-
-I3BlockHandler.print_json("#{current}", 'V', color)
--- .i3/scripts/time.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-require_relative 'lib/i3block_handler'
-
-time = `date "+%d-%m-%Y %H:%M:%S"`.chomp()
-
-I3BlockHandler.print_json(time, '00-00-1970 00:00:00')
